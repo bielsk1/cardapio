5b8a4bc67c0bb15529829b99d1e47b6a2f30ead7
bielsk1 <groovestreet903@proton.me>
2024-08-18 17:01:28 -0300

    1

---
+++ config.lua
@@ -0,0 +1,5 @@
+Config = {}
+
+Config.Locations = {
+	[1] = {NewsLocation = vector3(257.38, -631.322, 40.54)},
+}
---
+++ fxmanifest.lua
@@ -0,0 +1,20 @@
+fx_version 'cerulean'
+
+game 'gta5'
+
+version '1.1.0'
+
+ui_page 'client/html/index.html'
+
+client_script 'client/client.lua'
+
+shared_script 'config.lua'
+
+files {
+	'client/html/index.html',
+	'client/html/style.css',
+	'client/html/script.js',
+	'client/html/reset.css',
+	'client/html/*.jpg',
+	'client/html/*.png'
+}
